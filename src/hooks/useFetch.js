77451f9d869e49a5b8a86b637adb4336aa859d9b
ArobyR <ar.rosario001@gmail.com>
2021-06-29 09:29:09 -0400

    update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 
-const useFetch = (url, options={}) => {
+const useFetch = (url, options = {}) => {
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       try {
         const res = await fetch(url, options);
         const data = await res.json();
         setResponse(data);
+        setLoading(false);
       } catch (error) {
         setError(error);
       }
     };
     fetchData();
   }, []);
-  return {response, error};
+  return { response, error, loading };
 };
 
 export default useFetch;
