6f33a79275cc6179b346ac5751cbc53a3f48cee2
ArobyR <ar.rosario001@gmail.com>
2021-07-13 21:21:06 -0400

    update reducerCounter

--- src/reducers/reduceCounter.js
+++ src/reducers/reduceCounter.js
@@ -2,6 +2,9 @@ const reduceCounter = ({ state, actions }) => {
   switch (actions.type) {
     case "add":
       return state + 1;
+
+    case "subtraction":
+      return state - 1;
     default:
       return state;
   }
