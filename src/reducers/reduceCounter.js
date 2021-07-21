3818cc5967aaaa7bcee869c9619fbe1b03edd48b
ArobyR <ar.rosario001@gmail.com>
2021-07-21 13:53:16 -0400

    Update files
    Update reducer and useFetch

--- src/reducers/reduceCounter.js
+++ src/reducers/reduceCounter.js
@@ -1,10 +1,10 @@
 const reduceCounter = ({ state, actions }) => {
   switch (actions.type) {
     case "add":
-      return state.num + 1;
+      return state + 1;
 
     case "subtraction":
-      return state.num - 1;
+      return state - 1;
     default:
       return state;
   }
