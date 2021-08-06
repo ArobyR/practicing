09332b5abf2460bdec269078fa50c93cb58e094f
ArobyR <ar.rosario001@gmail.com>
2021-08-06 19:30:58 -0400

    add sass styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.scss";
+// import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { UserContext } from "./contexts/UserContext";
 import AppRouter from "./routes/AppRouter";
