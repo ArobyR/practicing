0fb4dc8f1a59fbcaa97a750eea76dd402628a3e0
ArobyR <ar.rosario001@gmail.com>
2021-08-06 19:30:40 -0400

    styling files

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,15 +7,17 @@ const About = () => {
   return (
     <div className="container text-center mt-4">
       <p>Page About</p>
-        {
-            !user ? <h2>Plase login...</h2> : (
-                <div>
-                    <h3>Name: {user.first_name} {user.last_name}</h3>
-                    <h3>Email: {user.email}</h3>
-                    <img src={user.avatar} alt={user.id} width="150" height="150" />
-                </div>
-            )
-        }
+      {!user ? (
+        <h2>Plase login...</h2>
+      ) : (
+        <div>
+          <h3>
+            Name: {user.first_name} {user.last_name}
+          </h3>
+          <h3>Email: {user.email}</h3>
+          <img src={user.avatar} alt={user.id} width="150" height="150" />
+        </div>
+      )}
     </div>
   );
 };
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "../components/Header";
 import HomeV from "../pages/HomeV";
 import About from "../pages/About";
-import NotFound from "../components/NotFound"
+import NotFound from "../components/NotFound";
 
 const AppRouter = () => {
   return (
